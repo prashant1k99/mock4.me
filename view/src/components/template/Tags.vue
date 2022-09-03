3d40df352378cc2e0785efb8497f158a34ad9738
Prashant <prashantco111@gmail.com>
2022-09-03 16:42:19 +0530

    WIP: Added Tag Component

---
+++ view/src/components/template/Tags.vue
@@ -0,0 +1,18 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <span
+    class="inline-flex items-center bg-gray-100 px-2.5 py-0.5 font-medium text-gray-800" 
+    :class="[rounded ? 'rounded-full': 'rounded-md', large? 'text-sm' : 'text-xs']" 
+    v-bind="$attrs">
+    <slot></slot>
+  </span>
+</template>
+
+<script>
+export default {
+  props: {
+    large: Boolean,
+    rounded: Boolean
+  }
+}
+</script>
