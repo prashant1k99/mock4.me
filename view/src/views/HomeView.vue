5477caa70df4ec180ca65d0f42584818c0a7d75b
Prashant <prashantco111@gmail.com>
2022-09-03 16:42:06 +0530

    WIP: Cleared the default files

--- view/src/views/HomeView.vue
+++ view/src/views/HomeView.vue
@@ -1,18 +1,20 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <StackList />
+    <Tags class="bg-yellow-100 text-yellow-800 border-5 " large >I Am Happy</Tags>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import StackList from '@/components/template/StackList.vue';
+import Tags from '../components/template/Tags.vue'; // @ is an alias to /src
 
 export default defineComponent({
   name: 'HomeView',
   components: {
-    HelloWorld,
-  },
+    StackList,
+    Tags
+},
 });
 </script>
